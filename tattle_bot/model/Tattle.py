0e79435bef899d487d87c81e0775e2796d2cabd8
Andrew Mickael <andrew.mickael@tdn2k.com>
2020-06-27 08:39:56 -0500

    Fixed non-mention bug

--- tattle_bot/model/Tattle.py
+++ tattle_bot/model/Tattle.py
@@ -1,11 +1,9 @@
 import time
-from typing import Union
 import logging
 
 from praw import Reddit
 from praw.exceptions import RedditAPIException
 from praw.models.reddit.comment import Comment
-from praw.models.reddit.message import Message
 
 from tattle_bot.model.Activity import Activity
 
@@ -28,37 +26,37 @@ class Tattle:
         )
 
     @staticmethod
-    def __process(handler: Activity, item: Union[Comment, Message]):
-        if isinstance(item, Comment):
-            requester = item.author
-            author = item.parent().author
-            message = handler.combined_formatted(author)
-            while True:
-                try:
-                    item.reply(message)
-                except RedditAPIException as e:
-                    # Log error
-                    logging.error(e)
+    def __process(handler: Activity, item: Comment):
+        requester = item.author
+        author = item.parent().author
+        message = handler.combined_formatted(author)
+        while True:
+            try:
+                item.reply(message)
+            except RedditAPIException as e:
+                # Log error
+                logging.error(e)
 
-                    # Initialize duration, multiplier, and exception
-                    duration = 30
-                    multiplier = 1
-                    exception = str(e)
+                # Initialize duration, multiplier, and exception
+                duration = 30
+                multiplier = 1
+                exception = str(e)
 
-                    # If error is a rate limit then wait the specified duration
-                    if "ratelimit" in exception:
-                        duration = int("".join([i for i in exception if i.isdigit()]))
-                        if "minute" in exception:
-                            multiplier = 60
-                    time.sleep(duration * multiplier + 5)
-                else:
-                    logging.info(f"Fetched info about /u/{author} for /u/{requester}")
-                    break
+                # If error is a rate limit then wait the specified duration
+                if "ratelimit" in exception:
+                    duration = int("".join([i for i in exception if i.isdigit()]))
+                    if "minute" in exception:
+                        multiplier = 60
+                time.sleep(duration * multiplier + 5)
+            else:
+                logging.info(f"Fetched info about /u/{author} for /u/{requester}")
+                break
 
     def listen(self, skip_existing: bool = True):
         handler = Activity(self.client)
         for item in self.client.inbox.stream(skip_existing=skip_existing):
             try:
-                self.__process(handler, item)
+                if isinstance(item, Comment) and item in self.client.inbox.mentions():
+                    self.__process(handler, item)
             except Exception as e:
                 logging.error(e)
